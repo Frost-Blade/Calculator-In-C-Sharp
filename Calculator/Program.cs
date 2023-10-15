42a8f2a4d6da8adf450e917b32b4b0310da291c5
Frost-Blade <130753523+Frost-Blade@users.noreply.github.com>
2023-10-15 17:22:16 +0800

    Changes 10-15-2023 -created useAgain bool to be used when asking the user if they want to use the program again -moved the main program inside a do while loop and used the useAgain bool as a parameter to use the program again or not -added Console.Clear(); if the user wants to use the program again to clear the console

--- Calculator/Program.cs
+++ Calculator/Program.cs
@@ -7,66 +7,83 @@ namespace CalculatorInCSharp
 
         public static void Main (string[] args)
         {
-            //declare and initialize variables to be used in the calculator
-            int a = 0;
-            int b = 0;
-            int c = 0;
+            bool useAgain = true; //variable to be used when asking the user if they want to use the app again
 
-            //provide information about the program and ask user to input a number based on the operation the user would like to use
-            Console.WriteLine("This is a calculator that calculates the value between two whole numbers\n" +
-                                "Please type the corresponding number (1 to 4) to select an operation: \n" +
-                                "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
-
-            //declare an int variable called operation to hold the value of the input from the user
-            int operation = Convert.ToInt32(Console.ReadLine());
-
-            //use a switch case and pass the int variable operation as a parameter to determine which operation will be used based on the user input
-            switch (operation)
+            do //use a do loop for the program to run atleast once
             {
-                case 1: 
-                    Console.WriteLine("\nYou chose Addition!\n");
-                    break; 
-                case 2:
-                    Console.WriteLine("\nYou chose Subtraction!\n");
-                    break;
-                case 3:
-                    Console.WriteLine("\nYou chose Multiplication!\n");
-                    break;
-                case 4:
-                    Console.WriteLine("\nYou chose Division!\n");
-                    break;
-                default: //created a default for values outside of definition (non 1, 2, 3, and 4 int inputs)
-                    Console.WriteLine("\nYou entered the wrong number!\nThe program will exit now");
-                    Console.ReadLine();
-                    return; //return exits the program
-            }
+                //declare and initialize variables to be used in the calculator
+                int a = 0;
+                int b = 0;
+                int c = 0;
 
-            
-            Console.WriteLine("Enter your first number: "); //ask the the user to input the first number
-            a = Convert.ToInt32(Console.ReadLine()); //read and convert it to int and then set the value to a
-            Console.WriteLine("Enter your second nubmer: "); //ask the the user to input the second number
-            b = Convert.ToInt32(Console.ReadLine()); //read and convert it to int and then set the value to b
+                //provide information about the program and ask user to input a number based on the operation the user would like to use
+                Console.WriteLine("This is a calculator that calculates the value between two whole numbers\n" +
+                                    "Please type the corresponding number (1 to 4) to select an operation: \n" +
+                                    "1. Addition\n2. Subtraction\n3. Multiplication\n4. Division\n");
 
-            //use a switch case and pass the variable operation as a parameter to determine what operation will be used and print the output
-            switch (operation)
-            {
-                case 1: 
-                    c = a + b;
-                    Console.WriteLine("\nThe sum of " + a + " + " + b + " is: " + c);
-                    break;
-                case 2:
-                    c = a - b;
-                    Console.WriteLine("\nThe difference of " + a + " - " + b + " is: " + c);
-                    break;
-                case 3:
-                    c = a * b;
-                    Console.WriteLine("\nThe product of " + a + " * " + b + " is: " + c);
-                    break;
-                case 4:
-                    c = a / b;
-                    Console.WriteLine("\nThe quotient of " + a + " / " + b + " is: " + c);
-                    break;
-            }
+                //declare an int variable called operation to hold the value of the input from the user
+                int operation = Convert.ToInt32(Console.ReadLine());
+
+                //use a switch case and pass the int variable operation as a parameter to determine which operation will be used based on the user input
+                switch (operation)
+                {
+                    case 1:
+                        Console.WriteLine("\nYou chose Addition!\n");
+                        break;
+                    case 2:
+                        Console.WriteLine("\nYou chose Subtraction!\n");
+                        break;
+                    case 3:
+                        Console.WriteLine("\nYou chose Multiplication!\n");
+                        break;
+                    case 4:
+                        Console.WriteLine("\nYou chose Division!\n");
+                        break;
+                    default: //created a default for values outside of definition (non 1, 2, 3, and 4 int inputs)
+                        Console.WriteLine("\nYou entered the wrong number!\nThe program will exit now");
+                        Console.ReadLine();
+                        return; //return exits the program
+                }
+
+
+                Console.WriteLine("Enter your first number: "); //ask the the user to input the first number
+                a = Convert.ToInt32(Console.ReadLine()); //read and convert it to int and then set the value to a
+                Console.WriteLine("Enter your second nubmer: "); //ask the the user to input the second number
+                b = Convert.ToInt32(Console.ReadLine()); //read and convert it to int and then set the value to b
+
+                //use a switch case and pass the variable operation as a parameter to determine what operation will be used and print the output
+                switch (operation)
+                {
+                    case 1:
+                        c = a + b;
+                        Console.WriteLine("\nThe sum of " + a + " + " + b + " is: " + c);
+                        break;
+                    case 2:
+                        c = a - b;
+                        Console.WriteLine("\nThe difference of " + a + " - " + b + " is: " + c);
+                        break;
+                    case 3:
+                        c = a * b;
+                        Console.WriteLine("\nThe product of " + a + " * " + b + " is: " + c);
+                        break;
+                    case 4:
+                        c = a / b;
+                        Console.WriteLine("\nThe quotient of " + a + " / " + b + " is: " + c);
+                        break;
+                }
+                Console.Write("\nDo you want to use the Calculator again? (Y/N): ");
+                String response = Console.ReadLine();
+                response.ToLower();
+                if (response == "y") 
+                {
+                    Console.Clear(); //if user wants to use the program again, clear the console and start the loop again
+                }
+                else //if user doesn't want to use the program again, set the useAgain bool variable to false and exit the loop
+                {
+                    useAgain = false;
+                    Console.WriteLine("Thank you for using the Calculator app!");
+                }
+            } while (useAgain);
             Console.ReadLine();
         }
     }
